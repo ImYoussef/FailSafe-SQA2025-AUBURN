05b02b0655efe8eb34a968f2e3e885cf7b8e923b
Youssef <94089080+ImYoussef@users.noreply.github.com>
2025-04-25 17:36:43 -0500

    added 2 methods to fuzz.py

--- KubeSec/KubeSec-master/fuzz.py
+++ KubeSec/KubeSec-master/fuzz.py
@@ -5,11 +5,14 @@ import string
 #from parserFuzzer import userNameFuzzer
 from graphtaintFuzzer import constructHelmStringFuzzer
 from myLogger import giveMeLoggingObject
+from parser import checkIfWeirdYAML
+from scanner import scanForDefaultNamespace
 #TODO: implement fuzzer
 
 def fuzzer():
     #create logger
     logger = giveMeLoggingObject()
+
     
     #userNameFuzzer()
     try:
@@ -21,6 +24,36 @@ def fuzzer():
         logger.error("An error occurred:\n", exc_info=True)
     return 0
 
+#Youssef's Methods
+def fuzzCheckIfWeirdYAML():
+    for i in range(10):
+        fuzz_input = random.choice([
+            123,
+            3.14,
+            None,
+            True,
+            [],
+            {},
+            set(),
+            ''.join(random.choices(string.printable, k=20))
+        ])
+        try:
+            checkIfWeirdYAML(fuzz_input)
+        except Exception as e:
+            print(f"[{i}] Exception Caught in checkIfWeirdYAML:\n\t EXCEPTION: {e}")
+
+def fuzzScanForDefaultNamespace():
+    for i in range(10):
+        fuzz_input = ''.join(random.choices(string.printable, k=15))
+        try:
+            scanForDefaultNamespace(fuzz_input)
+        except Exception as e:
+            print(f"[{i}] Exception Caught in scanForDefaultNamespace:\n\t EXCEPTION: {e}")
+
+def formatException(eText):
+    print(f"Exception Caught in function checkIfWeirdYAML from parser.py:\n\t EXCEPTION: {eText}")
+
 if __name__=='__main__':
     fuzzer()
-
+    fuzzCheckIfWeirdYAML()
+    fuzzScanForDefaultNamespace()
