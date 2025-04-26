6e3c660cf62235e84d579b73c246029980aec401
Adrian Rushing <adrianjrushing@gmail.com>
2025-04-26 14:53:03 -0500

    add local work and MLForensics directory

--- KubeSec/KubeSec-master/fuzz.py
+++ KubeSec/KubeSec-master/fuzz.py
@@ -1,59 +1,69 @@
-#TODO: import 5 python methods we want to fuzz
-#from scanner import isValidUserName
+# TODO: import 5 python methods we want to fuzz
+# from scanner import isValidUserName
+
+# from parserFuzzer import userNameFuzzer
 import random
 import string
-#from parserFuzzer import userNameFuzzer
+from parser import checkIfWeirdYAML
+
 from graphtaintFuzzer import constructHelmStringFuzzer
 from myLogger import giveMeLoggingObject
-from parser import checkIfWeirdYAML
 from scanner import scanForDefaultNamespace
-#TODO: implement fuzzer
+
+# TODO: implement fuzzer
+
 
 def fuzzer():
-    #create logger
+    # create logger
     logger = giveMeLoggingObject()
-
-    
-    #userNameFuzzer()
     try:
         logger.info("Logging constructHelmStringFuzzer() from graphtaint.py:")
-        logger.info("Result {}", constructHelmStringFuzzer())
-        logger.info("Logging getHelpTemplateContent() form graphtaint.py:")
-        logger.info("Result {}", getHelmTemplateContentFuzzer())
-    except Exception as e:
+        logger.info("Result %s", constructHelmStringFuzzer())
+
+    except Exception:
         logger.error("An error occurred:\n", exc_info=True)
     return 0
 
-#Youssef's Methods
+
+# Youssef's Methods
 def fuzzCheckIfWeirdYAML():
     for i in range(10):
-        fuzz_input = random.choice([
-            123,
-            3.14,
-            None,
-            True,
-            [],
-            {},
-            set(),
-            ''.join(random.choices(string.printable, k=20))
-        ])
+        fuzz_input = random.choice(
+            [
+                123,
+                3.14,
+                None,
+                True,
+                [],
+                {},
+                set(),
+                "".join(random.choices(string.printable, k=20)),
+            ]
+        )
         try:
             checkIfWeirdYAML(fuzz_input)
         except Exception as e:
             print(f"[{i}] Exception Caught in checkIfWeirdYAML:\n\t EXCEPTION: {e}")
 
+
 def fuzzScanForDefaultNamespace():
     for i in range(10):
-        fuzz_input = ''.join(random.choices(string.printable, k=15))
+        fuzz_input = "".join(random.choices(string.printable, k=15))
         try:
             scanForDefaultNamespace(fuzz_input)
         except Exception as e:
-            print(f"[{i}] Exception Caught in scanForDefaultNamespace:\n\t EXCEPTION: {e}")
+            print(
+                f"[{i}] Exception Caught in scanForDefaultNamespace:\n\t EXCEPTION: {e}"
+            )
+
 
 def formatException(eText):
-    print(f"Exception Caught in function checkIfWeirdYAML from parser.py:\n\t EXCEPTION: {eText}")
+    print(
+        f"Exception Caught in function checkIfWeirdYAML from parser.py:\n\t EXCEPTION: {eText}"
+    )
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     fuzzer()
     fuzzCheckIfWeirdYAML()
     fuzzScanForDefaultNamespace()
--- KubeSec/KubeSec-master/graphtaintFuzzer.py
+++ KubeSec/KubeSec-master/graphtaintFuzzer.py
@@ -1,9 +1,8 @@
 from graphtaint import constructHelmString, getHelmTemplateContent
-import random
-import string
+
 
 def constructHelmStringFuzzer():
-    #input non tuples
+    # input non tuples
     print("Fuzzing constructHelmString() from graphtaint.py")
     print("Trying input: 123", constructHelmString(123))
     print("Trying input: test", constructHelmString("test"))
@@ -11,7 +10,8 @@ def constructHelmStringFuzzer():
     print("Trying input: True", constructHelmString(True))
     print("Trying input: False", constructHelmString(False))
     print("Trying input: ()", constructHelmString(()))
-    
+
+
 def getHelmTemplateContentFuzzer():
     print("Fuzzing getHelmTemplateContent() from graphtaint.py")
     print("Trying bad file path", getHelmTemplateContent("not/a/file"))
