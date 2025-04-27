c183e7713a4283f64e78901b34c9d67c04ac048c
Adrian <102313708+adrianrushing@users.noreply.github.com>
2025-04-26 23:59:46 -0500

    Update fuzzer.py

--- KubeSec/KubeSec-master/fuzzer.py
+++ KubeSec/KubeSec-master/fuzzer.py
@@ -193,11 +193,3 @@ class UserNameFuzzer(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
--e 
-# Automated Comment
--e 
-# Automated Comment - 6:50PM
--e 
-# Automated Comment - 6:53PM
--e 
-# Automated Comment - 7:02PM
